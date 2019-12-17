ee78da53a13a729e8c00994c311b67b5fa80c97f
Bridger Middleton <bmiddy3@gmail.com>
2019-12-17 13:42:11 -0700

    swag

--- public/src/app/project/create-family/create-family.component.ts
+++ public/src/app/project/create-family/create-family.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
+import { Family } from "src/app/models/family.interface";
 
 @Component({
-  selector: 'app-create-family',
-  templateUrl: './create-family.component.html',
-  styleUrls: ['./create-family.component.scss']
+  selector: "app-create-family",
+  templateUrl: "./create-family.component.html",
+  styleUrls: ["./create-family.component.scss"]
 })
-export class CreateFamilyComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class CreateFamilyComponent {
+  newFamily: Family = { name: "" };
+  constructor() {}
 }
