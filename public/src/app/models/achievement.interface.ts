6035525907026232c7e08a132c582d83eaeb33ef
Ivar Brandt <ibrandt2@gmail.com>
2019-12-18 11:21:02 -0700

    homepagecomponent

--- public/src/app/models/achievement.interface.ts
+++ public/src/app/models/achievement.interface.ts
@@ -6,6 +6,7 @@ export interface Achievement {
   length_of_time: string;
   image_url?: string;
   _id?: string;
+  month: string;
   createdAt?: Date;
   updatedAt?: Date;
 }
--- server/config/routes.js
+++ server/config/routes.js
@@ -4,14 +4,8 @@ const FamilyController = require("../controllers/family.controller");
 module.exports = app => {
   app.get("/api/achievements", AchievementController.showAllAchievements);
   app.get("/api/achievement/:id", AchievementController.showOneAchievement);
-  app.post(
-    "/api/achievements/new/:id",
-    AchievementController.updateAchievement
-  );
-  app.delete(
-    "/api/achievements/delete/:id",
-    AchievementController.deleteAchievement
-  );
+  app.post("/api/achievements/new/:id", AchievementController.updateAchievement);
+  app.delete("/api/achievements/delete/:id", AchievementController.deleteAchievement);
   app.get("/api/families", FamilyController.allFamilies);
   app.get("/api/families/:id", FamilyController.findOneFamily);
   app.post("/api/families/new", FamilyController.createFamily);
